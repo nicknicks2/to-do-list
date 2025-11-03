524bd22ad9507713ef15a7671923bb428ed2d32d
nicknicks2 <nicolly.santos555@gmail.com>
2025-11-03 13:06:57 -0300

    change made

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-alert('ola mundo');
 
-
-const itemInput = document.getElementById('itemImput');
+const itemInput = document.getElementById('itemInput');
 const adicionarButton = document.getElementById('adicionarButton');
 const listaItens = document.getElementById('listaItens');
 
 adicionarButton.addEventListener('click', adicionarItem);
-
 function adicionarItem() {
  const textoItem = itemInput.value;
-
-    if (textoItem.trin() !== '')
-        {
+    if (textoItem.trim() !== ''){
             const novoItem = document.createElement('li');
             novoItem.textContent = textoItem;
 
             listaItens.appendChild(novoItem);
-
-
             itemInput.value = '';
             itemInput.focus();
 
